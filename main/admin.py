129cb61594a1f8698844753bcc7e412a0b5ccb42
oloshe <jiangxianhe@outlook.com>
2021-10-29 14:28:03 +0800

    fixing version history issues for merge

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Author, Comment, Post
+admin.site.register(Author)
+admin.site.register(Comment)
+admin.site.register(Post)

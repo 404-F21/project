a0bb087879d074f20b74b7a3a9027220dd2fc0f9
Warren <wstix@ualberta.ca>
2021-11-28 16:21:50 -0700

    cleaning up imports

--- main/decorator.py
+++ main/decorator.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Nathan Drapeza, Xingjie He, Warren Stix, Yifan Wu
+# Copyright 2021 Xingjie He, Yifan Wu
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

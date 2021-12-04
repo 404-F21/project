f80dcea2d210430f3664043ccc9b4d582fe144e9
Warren <wstix@ualberta.ca>
2021-12-04 16:21:49 -0700

    fixing followed endpoint

--- main/views.py
+++ main/views.py
@@ -164,7 +164,7 @@ class FollowerDetail(APIView):
     def get(self, request, pk, fpk, format=None):
         try:
             author = Author.objects.get(pk=uuid.UUID(pk))
-            author.followee_set.get(follower=uuid.UUID(fpk))
+            author.followed_set.get(follower=uuid.UUID(fpk))
             return Response({ 'isFollower': True })
         except (Author.DoesNotExist, Following.DoesNotExist):
             return Response({ 'isFollower': False })
@@ -173,7 +173,7 @@ class FollowerDetail(APIView):
 class FollowedList(APIView):
     def get(self, request, pk, format=None):
         author = Author.objects.get(pk=uuid.UUID(pk))
-        follow_pairs = author.followee_set.all().order_by('followee__displayName')
+        follow_pairs = author.followed_set.all().order_by('followee__displayName')
         paged_pairs = paginate(follow_pairs, request.query_params)
         serializer = FollowingSerializer(paged_pairs, many=True)
 
